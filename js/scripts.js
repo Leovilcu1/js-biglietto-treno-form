c7107bdea863cdbc2302025f696be6a0cc8f2607
LeonidVilcu <vilcu.leo@mail.ru>
2022-12-21 20:20:35 +0100

    js-biglietto-treno-form-completato

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,7 @@ const btnAnulla = document.getElementById("btn-anulla");
 
 btnGenera.addEventListener("click",
     function(){
-        console.log("nameInput.value" , nameInput.value ,typeof nameInput.value);
-        console.log("kmInput.value" , kmInput.value ,typeof kmInput.value);
-
         kmInput = parseInt(kmInput.value);
-        console.log("kmInput" , kmInput ,typeof kmInput);
-        
-        
 
         if(isNaN(kmInput)==true){
             alert("inserisci i valori validi, Aggiornare la paggine e riprovare")
@@ -20,37 +14,39 @@ btnGenera.addEventListener("click",
         else{
             let price = kmInput * 0.21;
             console.log("price" , price ,typeof price);
-
+            document.getElementById("costo-biglietto").innerHTML ="€" +  price;
             ageSelect = ageSelect.value;
-            console.log("ageSelect" , ageSelect ,typeof ageSelect);
 
-            const carozza = Math.floor(Math.random () * 20);
-            console.log("carozza" , carozza);
+            let carrozza = Math.floor(Math.random () * 20);
+            document.getElementById("carrozza").innerHTML = carrozza;
 
-            const codiceCP = Math.floor(Math.random () * 100000) + 1;
-            console.log("codiceCP" , codiceCP);
+            let codiceCp = Math.floor(Math.random () * 100000) + 1;
+            document.getElementById("codice-cp").innerHTML = codiceCp;
+
+            let offerta ="nulla";
+            document.getElementById("offerta").innerHTML = (offerta);
+
+            let discount = "prezzo pieno";
+            document.getElementById("sconto").innerHTML = discount;
 
             if (ageSelect == "min") {
                 let discount = price / 100 * 20;
-                console.log("discount" , discount);
+                document.getElementById("sconto").innerHTML ="€" +  discount;
                 price = price - discount;
-                console.log("price" , price , typeof price);
-                discount = "offerta" + " ridotto";
-                console.log(discount);
+                document.getElementById("costo-biglietto").innerHTML ="€" +  price;
+                let offerta ="ridotto";
+                document.getElementById("offerta").innerHTML = (offerta);
             }
             else if (ageSelect == "over") {
                 let discount = price / 100 * 40;
-                console.log("discount" , discount);
+                document.getElementById("sconto").innerHTML ="€" +  discount;
                 price = price - discount;
-                console.log("price" , price , typeof price);
-                discount = "silver";
-                console.log(discount);
+                document.getElementById("costo-biglietto").innerHTML = "€" + price;
+                let offerta = "silver";
+                document.getElementById("offerta").innerHTML = (offerta);
             }
-    
         }
-        
+        document.getElementById("nome-passeggero").innerHTML = nameInput.value;
+        document.getElementById("section-two").style.display = "block";
     }
-
-
-
 )
